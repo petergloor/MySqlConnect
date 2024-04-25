cd40cf9de0750ad3cf19d739742eea344eae8305
Peter Gloor <pgloor@gmail.com>
2024-04-25 12:25:34 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿/*
+    This is a simple console app to test connections to a 
+    OpenSimulator Robust Database on a MySql or MariaDB Server.
+
+    We connect to the database using the connString below and
+    query PrincipalID, FirstName and Lastname from UserAccounts.
+*/
+
+using System;
+using MySql.Data.MySqlClient;
+using Nini.Config;
+
+namespace MySqlConnect
+{
+    internal class Program
+    {  
+        static void Main(string[] args) 
+        {
+            IConfigSource source = new IniConfigSource("MySqlConnect.ini");
+            string connectionString = source.Configs["DatabaseService"].Get("ConnectionString");
+            Console.WriteLine("Connect to OpenSim MySQL Server, get user accounts and output id, first and last name.");
+            try
+            {
+                // Connect to database
+                var conn = new MySqlConnection(@connectionString);
+                conn.Open();
+                Console.WriteLine("Connected");
+        
+                // Declare command
+                MySqlCommand cmd = new ()
+                {
+                    CommandText = @"SELECT * FROM UserAccounts;",
+                    Connection = conn
+                };
+
+                // Execute the command and read the results.
+                using var reader = cmd.ExecuteReader();
+                {
+                    while (reader.Read())
+                    {
+                        // Get and output the commands to the console.
+                        var id = reader.GetString("PrincipalId");
+                        var FirstName = reader.GetString("FirstName");
+                        var LastName = reader.GetString("LastName");
+
+                        Console.WriteLine("{0} {1} {2}", id, FirstName, LastName);
+                    }
+                }
+
+                // Close the connection when we are done
+                conn.Close();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                // Oops... an error occured.
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("Done :)");
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# MySqlConnect - a simple MySql connect example for OpenSimulator
+
+The purpose of this program is to provide a simmple console application program to test MySql connections for OpenSimulator on various platforms. It should provide an easier way to isolate and locate MySql Connection issues.
+
+The application connects to the OpenSim database on MySql or MariaDB Server. It reads the data from the UserAccounts table and returns the PrincipalId, FirstName and LastName for output to the console.
+
+### Connection String
+The connection string must be specified in MySqlConnect.ini in exactly the same way as in OpenSimulator.
+
+### Dependencies:
+
+Unlike OpenSimulator, we use Nuget packages. The following packages must be installed using `dotnet add package`.
+
+    dotnet add package MySql.Data --version 8.3.0
+    dotnet add package nini-core --version 0.9.2.16
+
+    
